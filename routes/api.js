83fdfc8430d1cfc850b2f9c66fbafff044d66d91
botstyle1 <84095101+botstyle1@users.noreply.github.com>
2021-06-01 03:50:57 +0700

    Update api.js

--- routes/api.js
+++ routes/api.js
@@ -2147,7 +2147,6 @@ router.get('/kuis/tebakbendera', async (req, res, next) => {
         var result = data;
         var result = data[Math.floor(Math.random() * data.length)];
              res.json({
-               results
              })
          })
          .catch(e => {
@@ -2170,7 +2169,6 @@ router.get('/kuis/family100', async (req, res, next) => {
         var result = data;
         var result = data[Math.floor(Math.random() * data.length)];
              res.json({
-                results
              })
          })
          .catch(e => {
